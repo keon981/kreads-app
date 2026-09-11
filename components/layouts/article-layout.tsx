c43aeb69642adf7fe2f3fa45a72641070c68b044
keon <h21816595@gmail.com>
2026-09-11 15:17:45 +0800

    🌈 style: sidebar style

--- components/layouts/article-layout.tsx
+++ components/layouts/article-layout.tsx
@@ -2,11 +2,31 @@ import React from 'react'
 
 import { RiGithubFill } from '@remixicon/react'
 
-import { CommentHeader } from '../blocks/comment'
+import { AboutCard } from '../blocks/about-card'
 import { SignInButton, SignInCard } from '../blocks/sign-in'
-import AppHeader from './app-header'
 
-interface Props extends React.ComponentProps<typeof CommentHeader> {
+function AppHeader() {
+  return (
+    <header
+      className="sticky top-0 z-20 h-18 w-full flex shrink-0 items-center gap-2 bg-background/95 md:bg-background"
+    >
+      {/* bottom */}
+      <div className="absolute bottom-0 left-2 w-[98%] md:border-b border-border"></div>
+
+      {/* bottom left */}
+      <div className="hidden md:block absolute top-15 -left-3 size-9 overflow-hidden">
+        <div className="absolute top-2.75 left-3 size-12.5 md:border-t md:border-l border-border rounded-3xl shadow-[0_0_0_12px] shadow-background"></div>
+      </div>
+
+      {/* bottom right */}
+      <div className="hidden md:block absolute overflow-hidden size-9 top-15 -right-3">
+        <div className="absolute top-2.75 right-3 size-12.5 md:border-t md:border-r border-border rounded-3xl shadow-[0_0_0_12px] shadow-background"></div>
+      </div>
+    </header>
+  )
+}
+
+interface Props extends React.ComponentProps<typeof AboutCard> {
   isAuth: boolean
 }
 
@@ -17,14 +37,14 @@ export default function ArticleLayout({
 }: Props) {
   return (
     <>
-      <div className="hidden md:block md:size-px"></div>
+      <div className="hidden md:block md:size-px" />
       <section className="relative w-full md:w-160 md:max-w-160 flex flex-col items-center min-h-dvh md:pb-18 md:bg-card">
         <AppHeader />
         <article className="size-full flex flex-col">
           <div className="grow min-h-0 overflow-hidden rounded-3xl md:border md:border-t-0 border-border">
-            <CommentHeader {...props}>
+            <AboutCard {...props}>
               {/* bio */}
-            </CommentHeader>
+            </AboutCard>
             {/* main post */}
             {children}
           </div>
